4f387843d233b6c5ec501ad75397b1c22491f733
Andrei <a.parchukov@yandex.ru>
2022-09-20 15:45:04 +0300

    Initial Commit

---
+++ UIKitSUI/UIKitSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitSUIApp.swift
+//  UIKitSUI
+//
+//  Created by Андрей Парчуков on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIKitSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
